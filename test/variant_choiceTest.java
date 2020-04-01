4891d771cab26e15384c5585549d9678a8e811e1
Arina <arina.odnoblyudova@mail.ru>
2020-04-01 16:32:17 +0300

    This is SQL to MongoDB project

--- test/variant_choiceTest.java
+++ test/variant_choiceTest.java
@@ -56,6 +56,7 @@ public class variant_choiceTest {
          { "SELECT * FROM sales LIMIT 10", "db.sales.find({}).limit(10)" },
          { "SELECT name, surname FROM collection" , "db.collection.find({}, {name: 1, surname: 1})" },
          { "SELECT * FROM collection SKIP 5 LIMIT 10", "db.collection.find({}).skip(5).limit(10)" },
+         { "SELECT * FROM customers WHERE age > 22 AND name = 'Vasya'", "db.customers.find({age: {$gt: 22}, name: {$eq: 'Vasya'}})" },
          { "SELECT * FROM customers WHERE age > 22", "db.customers.find({age: {$gt: 22}})" },
          { "SELECT name, surname, age, city FROM collection WHERE name <> Arina SKIP 2 LIMIT 7", "db.collection.find({name: {$ne: Arina}}, {name: 1, surname: 1, age: 1, city: 1}).skip(2).limit(7)" },
          { "SELECT age, city FROM collection WHERE age = 27", "db.collection.find({age: {$eq: 27}}, {age: 1, city: 1})"},
